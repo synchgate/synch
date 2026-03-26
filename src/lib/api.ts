b529c178f00b5861caf01a541f49035091c4a510
omash <omarsunday0@gmail.com>
2026-03-26 13:36:55 +0100

    added env file and base url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://payinfraterminal.onrender.com/v1",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
