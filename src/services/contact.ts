aef0af719a8df023d7df3e4340b53a9baa63ed58
omash <omarsunday0@gmail.com>
2026-04-10 14:15:34 +0100

    added see a demo

--- src/services/contact.ts
+++ src/services/contact.ts
@@ -8,9 +8,22 @@ export interface ContactPayload {
   contact_me: boolean;
 }
 
+export interface DemoPayload {
+  name: string;
+  email: string;
+  phone_number: string;
+  company_name: string;
+  preferred_date: string;
+  message: string;
+}
+
 export const contactService = {
   submitContact: async (data: ContactPayload) => {
     const response = await api.post("/communications/contact/", data);
+    return response.data;
+  },
+  submitDemoRequest: async (data: DemoPayload) => {
+    const response = await api.post("/communications/demo-sessions/", data);
     return response.data;
   },
 };
