37e9df556674816ccaa48d0b6cde9d98d593f33b
omash <omarsunday0@gmail.com>
2026-04-10 14:22:06 +0100

    added support ticket api

--- src/services/contact.ts
+++ src/services/contact.ts
@@ -17,7 +17,6 @@ export interface DemoPayload {
   message: string;
 }
 
-<<<<<<< HEAD
 export interface SupportPayload {
   support_type: string;
   subject: string;
@@ -28,8 +27,6 @@ export interface SupportPayload {
   message: string;
 }
 
-=======
->>>>>>> aef0af7 (added see a demo)
 export const contactService = {
   submitContact: async (data: ContactPayload) => {
     const response = await api.post("/communications/contact/", data);
@@ -39,11 +36,8 @@ export const contactService = {
     const response = await api.post("/communications/demo-sessions/", data);
     return response.data;
   },
-<<<<<<< HEAD
   submitSupportTicket: async (data: SupportPayload) => {
     const response = await api.post("/communications/support/", data);
     return response.data;
   },
-=======
->>>>>>> aef0af7 (added see a demo)
 };
